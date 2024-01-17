b84f31f7f66fce152f5eec513a5769852d7a2efe
CaydenLaptop <cayden.odegard@gmail.com>
2024-01-16 21:18:18 -0600

    add abt me page and seperated css

---
+++ aboutme.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CS3270</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="portfolio.css">
+</head>
+<body>
+	<header class="header">
+		<div>
+      <nav class="nav">
+        <ul class="nav-list">
+          <a class="nav-item" href="index.php">Home <br></a>
+          <a class="nav-item" href="aboutme.php">About <br></a>
+          <a class="nav-item" href="assignments.php">Assigments <br></a>
+          <a class="nav-item" href="https://github.com/kdqn/Advanced-Web">GitHub <br></a>
+        </ul>
+      </nav>
+		</div>
+<h1>About Me</h1>
+
+	</header>
+
+	<main class="container">
+		<article class="article">
+
+		<p>
+      What would you like to know about me?
+
+		</p>
+	
+	</article>
+</main>
+
+	
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CS3270</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="portfolio.css">
+</head>
+<body>
+	<header class="header">
+		<div>
+      <nav class="nav">
+        <ul class="nav-list">
+          <a class="nav-item" href="index.php">Home <br></a>
+          <a class="nav-item" href="aboutme.php">About <br></a>
+          <a class="nav-item" href="assignments.php">Assigments <br></a>
+          <a class="nav-item" href="https://github.com/kdqn/Advanced-Web">GitHub <br></a>
+        </ul>
+      </nav>
+		</div>
+<h1>Cayden Odegard</h1>
+
+	</header>
+
+	<div class="container">
+		<article class="article">
+
+		<p>
+
+			<table>
+				<tr>
+				<th    class="altTable2"><strong>Why am I taking this class?</strong></th>
+				<td   class="altTable">I am taking this class because I want to be able to create better and more useful websites and utilize the skills better, as well as this counting towards <br>
+					the requirements of the Computer Science degree I am working towards.</td>
+			  </tr>
+			  <tr>  
+				<th    class="altTable"><strong>What do I want to take away from this class?</strong></th>
+				<td    class="altTable2">I want to take away a better understanding of php, and just coding in general.</td>
+			  </tr>
+			  </table>
+
+		</p>
+	
+	</article>
+	</div>
+
+	
+
+	<script src="script.js"></script>
+</body>
+</html>
