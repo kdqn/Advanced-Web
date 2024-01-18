8b8fad7971051bccf0c5147a7703edea2cca7b50
CaydenLaptop <cayden.odegard@gmail.com>
2024-01-18 17:31:00 -0600

    fix css ref

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet">
-  <link rel="stylesheet" href="portfolio.css">
+  <link rel="stylesheet" href="Advanced-Web/portfolio.css">
 
 </head>
 <body>
